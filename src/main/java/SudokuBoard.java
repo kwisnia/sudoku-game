9cee28c560d5a768cf698f02d9760f7f4e9d187d
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2020-10-19 14:17:12 +0200

    Fill first square

--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SudokuBoard {
     private final int[][] sudokuBoard = new int[9][9];
 
@@ -5,7 +7,12 @@ public class SudokuBoard {
         return sudokuBoard;
     }
 
-    public static void fillBoard() {
-
+    public void fillBoard() {
+        Random random = new Random();
+        for (int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                sudokuBoard[i][j] = random.nextInt();
+            }
+        }
     }
 }
