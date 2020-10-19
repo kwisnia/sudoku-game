4a6e6c7ee74cbc8675b6ed4c0c8ce5246895d782
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2020-10-19 14:57:58 +0200

    Implement toString method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,7 @@
 public class App {
-
     public static void main(String[] args) {
         SudokuBoard su = new SudokuBoard();
         su.fillBoard();
         System.out.println(su.toString());
     }
-
 }
--- src/main/java/SudokuBoard.java
+++ src/main/java/SudokuBoard.java
@@ -8,10 +8,11 @@ public class SudokuBoard {
     }
 
     public void fillBoard() {
-        Random random = new Random();
+        int fill = 1;
         for (int i = 0; i <= 2; i++){
             for (int j = 0; j <= 2; j++){
-                sudokuBoard[i][j] = random.nextInt(9 - 1);
+               sudokuBoard[i][j] = fill;
+               fill++;
             }
 
         }
@@ -19,7 +20,26 @@ public class SudokuBoard {
 
     @Override
     public String toString() {
-        return Arrays.toString(sudokuBoard);
+        StringBuilder stringBuilder = new StringBuilder();
+        final String HORIZONTAL_BREAK = "-------------------------\n";
+        stringBuilder.append(HORIZONTAL_BREAK);
+        for (int i = 0; i < 9; i++) {
+            if (i == 3 || i == 6) {
+                stringBuilder.append(HORIZONTAL_BREAK);
+            }
+            stringBuilder.append("| ");
+            for (int j = 0; j < 9; j++) {
+                stringBuilder.append(sudokuBoard[i][j])
+                        .append(" ");
+                if (j == 2 || j == 5) {
+                    stringBuilder.append("| ");
+                }
+            }
+            stringBuilder.append("|\n");
+
+        }
+        stringBuilder.append(HORIZONTAL_BREAK);
+        return stringBuilder.toString();
     }
 
     public boolean validRow(int row) {
