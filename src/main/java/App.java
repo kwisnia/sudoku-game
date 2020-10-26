88e57f58d4095213c12af3afc52cd2a951904dc8
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2020-10-26 18:00:00 +0100

    Delete App

--- src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-
-    public static void main(String[] args) {
-        SudokuBoard su = new SudokuBoard(new BacktrackingSudokuSolver());
-        su.solveGame();
-        System.out.println(su.toString());
-    }
-}
