9f52ad88055c3c358917597d9d0db60790aeca6f
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2020-11-02 13:48:13 +0100

    Add SudokuField

---
+++ src/main/java/SudokuField.java
@@ -0,0 +1,15 @@
+public class SudokuField {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public SudokuField(int value) {
+        this.value = value;
+    }
+}
