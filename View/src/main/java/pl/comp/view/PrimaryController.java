d57debec51bc1ceb92ed9fb140d728f1146e5ac0
230025 <230025@edu.p.lodz.pl>
2020-12-07 14:22:00 +0100

    Adding options to choice box

--- View/src/main/java/pl/comp/view/PrimaryController.java
+++ View/src/main/java/pl/comp/view/PrimaryController.java
@@ -1,12 +1,16 @@
 package pl.comp.view;
 
 import java.io.IOException;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
 public class PrimaryController {
 
     public Button primaryButton;
+    public ChoiceBox difficultyChoiceBox;
 
     @FXML
     private void switchToSecondary() throws IOException {
