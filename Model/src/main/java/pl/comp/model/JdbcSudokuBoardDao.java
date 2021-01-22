60c143bb3148cbc901eb47f5ba29f8e948624e58
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2021-01-22 18:26:57 +0100

    Implement DatabaseDao read

--- Model/src/main/java/pl/comp/model/JdbcSudokuBoardDao.java
+++ Model/src/main/java/pl/comp/model/JdbcSudokuBoardDao.java
@@ -6,12 +6,13 @@ import java.util.ResourceBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.transform.Result;
+
 public class JdbcSudokuBoardDao implements Dao<SudokuBoard> {
     private final String name;
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String DRIVER = "org.postgresql.Driver";
-    private Statement jdbcStatement;
-    private Connection connection;
+    private final Connection connection;
     private final ResourceBundle bundle = ResourceBundle.getBundle("Exceptions");
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -27,6 +28,19 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard> {
         }
     }
 
+    private void createTables() throws SQLException {
+        try (Statement jdbcStatement = connection.createStatement()) {
+            String boardTables = "CREATE TABLE BOARDS(BOARD_ID int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, "
+                    + "BOARD_NAME varchar(30) NOT NULL); CREATE TABLE FIELDS(ROW int NOT NULL, COLUMN int NOT NULL, "
+                    + "VALUE int NOT NULL, BOARD_ID int, FOREIGN KEY (BOARD_ID) REFERENCES BOARDS (BOARD_ID) ON"
+                    + " UPDATE CASCADE ON DELETE CASCADE )";
+            jdbcStatement.executeUpdate(boardTables);
+            logger.debug("Utworzono tablice");
+        } catch (SQLException e) {
+            logger.error("Nie stworzono tablicy poprawnie");
+        }
+    }
+
     @Override
     public void close() throws Exception {
 
@@ -34,7 +48,34 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard> {
 
     @Override
     public SudokuBoard read() throws IOException, ClassNotFoundException, DaoException {
-        return null;
+        SudokuBoard readBoard = new SudokuBoard(new BacktrackingSudokuSolver());
+        int id;
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT BOARDS.BOARD_ID FROM " +
+                "BOARDS where BOARDS.BOARD_NAME=?"); ResultSet resultSet = preparedStatement.executeQuery()) {
+            if (resultSet.next()) {
+                id = resultSet.getInt(1);
+            } else {
+                logger.error("No nie");
+                throw new DaoException("placeholder");
+            }
+            logger.debug("Wczytano tablice");
+        } catch (SQLException e) {
+            throw new DaoException("placeholder");
+        }
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT FIELDS.ROW, " +
+                "FIELDS.COLUMN, FIELDS.VALUE FROM FIELDS WHERE FIELDS.BOARD_ID=?");
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                readBoard.set(resultSet.getInt(1),
+                        resultSet.getInt(2),
+                        resultSet.getInt(3));
+            }
+            logger.debug("Przypisano liczby");
+        } catch (SQLException e) {
+            logger.error("Test");
+            throw new DaoException("Test");
+        }
+        return readBoard;
     }
 
     @Override
