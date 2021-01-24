20aca45a8336b682e708226bf739c3b2ddc12157
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2021-01-24 23:05:40 +0100

    Rewrite transactions and set autocommit to false

--- Model/src/main/java/pl/comp/model/Dao.java
+++ Model/src/main/java/pl/comp/model/Dao.java
@@ -1,14 +1,12 @@
 package pl.comp.model;
 
-import java.io.IOException;
-import java.sql.SQLException;
 
 public interface Dao<T> extends AutoCloseable {
 
     @Override
     void close() throws Exception;
 
-    T read() throws IOException, ClassNotFoundException, DaoException, SQLException;
+    T read() throws Exception;
 
-    void write(T obj) throws DaoException, SQLException;
+    void write(T obj) throws Exception;
 }
